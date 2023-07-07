f9ad104a063585a67d0d6c7fb96597cb1fe25ced
Melvin-M-George <melvinmgeorge1234@gmail.com>
2023-07-07 18:45:22 +0530

    File added

---
+++ src/com/company/CWH_Ch2_Operators.java
@@ -0,0 +1,26 @@
+package com.company;
+
+public class CWH_Ch2_Operators {
+    public static void main(String[] args) {
+        //1.Arithmetic operators
+        int a = 4;
+//        int b = 6 % a; //Modulo Operator
+        //2.Assignment operators
+        int b = 9;
+        b += 3;
+        System.out.println(b);
+
+        //3.Comparison operator
+        // System.out.println(64>6);
+
+        //4.Logical operator
+        System.out.println(64>6 && 64>98);
+        System.out.println(64>6 || 64>98);
+
+        //5.Bitwise operators
+        System.out.println(2&3);
+
+        // 4.8%1.1 ---> Gives Decimal Remainder
+    }
+
+}

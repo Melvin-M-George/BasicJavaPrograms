fab6dfa81ece47d1d39b511a12fbe3e67193c15c
Melvin-M-George <melvinmgeorge1234@gmail.com>
2023-07-10 02:31:23 +0530

    File added

---
+++ src/com/company/CWH_14_String_Methods.java
@@ -0,0 +1,56 @@
+package com.company;
+
+import java.sql.SQLOutput;
+
+public class CWH_14_String_Methods {
+    public static void main(String[] args) {
+        String name = "MELVIN";
+        System.out.println(name);
+//        int value = name.length();
+//        System.out.println(value);
+
+//        String lowerstring = name.toLowerCase();
+//        System.out.println(lowerstring);
+//
+//        String upperstring = name.toUpperCase();
+//        System.out.println(upperstring);
+//
+//        String nonTrimmedString = "      Melvin M George       ";
+//        System.out.println(nonTrimmedString);
+//
+//        String trimmedString = nonTrimmedString.trim();
+//        System.out.println(trimmedString);
+
+//        System.out.println(name.substring(1));
+//        System.out.println(name.substring(1,4));
+/*
+        String name1 = "Harry";
+        System.out.println(name1);
+        System.out.println(name1.replace('r','p'));
+        System.out.println(name1.replace("arry","HHHH"));
+
+
+ */
+//        System.out.println(name.startsWith("Mel"));
+//        System.out.println(name.startsWith("vin"));
+
+//        System.out.println(name.endsWith("in"));
+
+//        System.out.println(name.charAt(2));
+
+        //System.out.println(name.indexOf('i'));
+
+//        String modifiedName = "Harryrry";
+//        System.out.println(modifiedName);
+//        System.out.println(modifiedName.indexOf("rry"));
+//        System.out.println(modifiedName.indexOf("rryrgfdsdv"));
+//        System.out.println(modifiedName.indexOf("rry",4));
+
+//        System.out.println(modifiedName.lastIndexOf('r'));
+
+//        System.out.println(name.equals("Harry"));
+        System.out.println(name.equalsIgnoreCase("melvin"));
+
+        System.out.println("This is escape \n sequence");
+    }
+}

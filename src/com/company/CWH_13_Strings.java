6bc2c07c325116c742dedce1dc411d1399aaeef4
Melvin-M-George <melvinmgeorge1234@gmail.com>
2023-07-09 18:33:36 +0530

    File added

---
+++ src/com/company/CWH_13_Strings.java
@@ -0,0 +1,19 @@
+package com.company;
+import java.util.Scanner;
+public class CWH_13_Strings {
+    public static void main(String[] args) {
+        //String name = new String("Melvin");
+        //String name = "Melvin";
+        //System.out.print("My name is : ");
+        //System.out.println(name);
+        int a = 6;
+        float b = 5.45454f;
+        System.out.printf("The value of a is %d and value of b is %4.2f",a,b);
+        //System.out.format("The value of a is %d and value of b is %f",a,b);
+        //Scanner sc = new Scanner(System.in);
+        //String str = sc.next();
+        //String str = sc.nextLine();
+        // System.out.println(str);
+
+    }
+}

5d4084dbd0cd4ec92b268924081ed0648132d0b6
Melvin-M-George <melvinmgeorge1234@gmail.com>
2023-07-10 18:32:13 +0530

    !!!

---
+++ src/com/company/CWH_15_PS3.java
@@ -0,0 +1,31 @@
+package com.company;
+
+public class CWH_15_PS3 {
+    public static void main(String[] args) {
+        //Problem-1
+        //String name = "Jack Parker";
+        //name = name.toLowerCase();
+        //System.out.println(name);
+
+        //Problem-2
+        //String text = "To Lower Case";
+        //text = text.replace(" ","_");
+        //System.out.println(text);
+
+        //Problem-3
+//        String letter = "Dear <|name|>, Thanks a lot";
+//        letter = letter.replace("<|name|>","John");
+//        System.out.println(letter);
+
+        //Problem-4
+//        String myString = "This string  contain double space and   tripple space";
+//        System.out.println(myString.indexOf("  "));
+//        System.out.println(myString.indexOf("   "));
+
+        //Problem-5
+        String myLetter = "Dear Harry,\n\t This Java course is nice.\n\t\t Thanks";
+        System.out.println(myLetter);
+
+
+    }
+}

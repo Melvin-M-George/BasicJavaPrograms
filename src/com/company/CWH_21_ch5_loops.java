da8f7dc0fcdb189cdf0b9d352f5d7974bf3d1e61
Melvin-M-George <melvinmgeorge1234@gmail.com>
2023-07-15 22:06:57 +0530

    Rock-Paper-Scissor-Game!

---
+++ src/com/company/CWH_21_ch5_loops.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class CWH_21_ch5_loops {
+    public static void main(String[] args) {
+        int i = 1;
+        while(i<=10){
+            System.out.println(i);
+            i++;
+        }
+        System.out.println("Finish running while loop");
+//        while(true){
+//            System.out.println("This is an infinite while loop!");
+//        }
+
+
+    }
+}

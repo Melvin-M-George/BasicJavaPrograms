a9e104ee889fcdd6ae9f5b210ff235f2815be871
Melvin-M-George <melvinmgeorge1234@gmail.com>
2023-07-16 19:40:39 +0530

    do_while

---
+++ src/com/company/CWH_22_do_while.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class CWH_22_do_while {
+    public static void main(String[] args) {
+
+//        int b = 10;
+//        do{
+//            System.out.println(b);
+//            b++;
+//        }while (b<5);
+//        System.out.println(b);
+
+        int a = 1;
+        do{
+            System.out.println(a);
+            a++;
+        }while (a<=30);
+
+
+    }
+}

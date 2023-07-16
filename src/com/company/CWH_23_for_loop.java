8ea5f89d8ed5c8ca6839970bf1966a2a5a1c9482
Melvin-M-George <melvinmgeorge1234@gmail.com>
2023-07-16 21:23:27 +0530

    for_loop

---
+++ src/com/company/CWH_23_for_loop.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class CWH_23_for_loop {
+    public static void main(String[] args) {
+//        for(int i = 0;i<=10;i++){
+//            System.out.println(i);
+//        }
+
+
+        for ( int i = 0; i < 10; i++){
+            System.out.println(2*i+1);
+        }
+    }
+}

e3cb4f3025f0e7b638f58b4ef30fcec2145c629f
Melvin-M-George <melvinmgeorge1234@gmail.com>
2023-07-17 18:49:26 +0530

    break and continue

---
+++ src/com/company/CWH_24_break_and_continue.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class CWH_24_break_and_continue {
+    public static void main(String[] args) {
+        for(int i = 0;i<5;i++){
+            System.out.println(i);
+            if (i==3){
+                System.out.println("The loop ends here");
+                break;
+            }
+        }
+    }
+}

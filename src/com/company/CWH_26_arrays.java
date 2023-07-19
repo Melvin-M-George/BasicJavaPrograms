5f25e1f25cdaf880d999b986413f95016a6c2a2c
Melvin-M-George <melvinmgeorge1234@gmail.com>
2023-07-19 17:50:22 +0530

    Arrays

--- src/com/company/CWH_26_arrays.java
+++ src/com/company/CWH_26_arrays.java
@@ -2,12 +2,15 @@ package com.company;
 
 public class CWH_26_arrays {
     public static void main(String[] args) {
-        int [] marks = new int[5];
-        marks[0]=100;
-        marks[1]=90;
-        marks[2]=85;
-        marks[3]=70;
-        marks[4]=80;
-        System.out.println(marks[4]);
+//        int [] marks = new int[5];
+//        marks[0]=100;
+//        marks[1]=90;
+//        marks[2]=85;
+//        marks[3]=70;
+//        marks[4]=80;
+//        System.out.println(marks[4]);
+
+        int [] marks = {100,90,85,70,80};
+        System.out.println(marks[2]);
     }
 }

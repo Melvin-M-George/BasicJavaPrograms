0a588042382a9d3812af040bb9a09eb9bd4844d8
Melvin-M-George <melvinmgeorge1234@gmail.com>
2023-07-19 17:43:00 +0530

    Arrays

---
+++ src/com/company/CWH_26_arrays.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class CWH_26_arrays {
+    public static void main(String[] args) {
+        int [] marks = new int[5];
+        marks[0]=100;
+        marks[1]=90;
+        marks[2]=85;
+        marks[3]=70;
+        marks[4]=80;
+        System.out.println(marks[4]);
+    }
+}

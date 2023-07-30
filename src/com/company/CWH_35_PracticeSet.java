3afba045c68bb4501211d900eb639e8b16f68fa7
Melvin-M-George <melvinmgeorge1234@gmail.com>
2023-07-30 22:00:49 +0530

    !

--- src/com/company/CWH_35_PracticeSet.java
+++ src/com/company/CWH_35_PracticeSet.java
@@ -2,11 +2,9 @@ package com.company;
 
 public class CWH_35_PracticeSet {
     static int multi_table(int a){
-        for(int i=1;i<=10;i++){
-
-        }
     }
     public static void main(String[] args) {
+
 
     }
 }

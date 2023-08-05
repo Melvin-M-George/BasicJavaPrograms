a8a2542f41ab18490b6e9ad08f1abb07b7f17983
Melvin-M-George <melvinmgeorge1234@gmail.com>
2023-08-05 13:23:59 +0530

    !

--- src/com/company/CWH_12_ch2_PS2_Q2.java
+++ src/com/company/CWH_12_ch2_PS2_Q2.java
@@ -6,7 +6,6 @@ public class CWH_12_ch2_PS2_Q2 {
         grade = (char)(grade + 8);
         System.out.println(grade);
 
-
         //Decrypt
         grade = (char)(grade - 8);
         System.out.println(grade);
